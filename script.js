d64bd6d17392228c510ad585c094af07d590761a
tparsons <tparsons@godaddy.com>
2024-09-20 15:36:12 -0700

    added new sidepanel

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function clickButton() {
+  console.log('Clicked the button');
+  // fetch('https://jsonplaceholder.typicode.com/todos/1')
+  // .then(response => response.json())
+  // .then(json => console.log(json));
+}
+
+// document.getElementById('myButton').addEventListener('click', clickButton);
